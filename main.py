768d74811efd8ad929e59ba4501d130db68ea27e
JackmanAlex228 <jackman.alex.w@gmail.com>
2025-08-25 11:53:17 -0600

    new loader class, more features

--- main.py
+++ main.py
@@ -1,46 +1,71 @@
 from atproto import Client
-from os import getenv
+from dotenv import load_dotenv 
+from os import getenv, listdir
+from loader import Loader
 import requests
+load_dotenv()
 
 def main():
+
+  # Load environment variables
   HANDLE = getenv('HANDLE')
   PASSWORD = getenv('PASSWORD')
   DOWNLOAD_PATH = getenv('DOWNLOAD_PATH')
   LIMIT = getenv('LIMIT')
 
+  # Initialize client
   client = Client()
   client.login(HANDLE, PASSWORD)
   print(f'Logged in as {client.me.display_name}')
 
+  # Initialize variables
   cursor = None
   all_likes = []
+  duplicates = []
+  num_fetched = 0
 
-  while True:
-    resp = client.app.bsky.feed.get_actor_likes({'actor': HANDLE, 'cursor': cursor})
-    all_likes.extend(resp.feed)
-    if not resp.cursor:
-      break
-    cursor = resp.cursor
-    print(f"Fetched {len(resp.feed)} likes, total so far: {len(all_likes)}")
-    if len(resp.feed) == 0:
-      print("No more likes to fetch.")
-      break
+  # Fetch all likes, displaying a status bar in the console
+  print(f"Fetching liked posts for {HANDLE}...")
+  with Loader(f"Fetching liked posts..."):
+    while True:
+      resp = client.app.bsky.feed.get_actor_likes({'actor': HANDLE, 'cursor': cursor})
+      all_likes.extend(resp.feed)
+      if not resp.cursor:
+        break
+      cursor = resp.cursor
+      if len(resp.feed) == 0:
+        break
+      num_fetched += len(resp.feed)
+      print(f" ({num_fetched} liked posts so far)", end='')
+    print(f"Total liked posts: {len(all_likes)}\n")
 
+  # Download images
   print(f"Total liked posts: {len(all_likes)}\n")
   print(f"Now downloading {LIMIT} images")
-  for item in all_likes[:LIMIT]:
+  for item in all_likes[:int(LIMIT)]:
     try:
       if hasattr(item.post.embed, 'images'):
         image_link = item.post.embed.images[0].fullsize
       if hasattr(item.post.embed, 'media'):
         image_link = item.post.embed.media.images[0].fullsize
+        image_link = item.post.embed.media.images[0].fullsize
     except (IndexError, AttributeError):
       print(f"No image found for post {item.post.uri}")
       continue
-    print(f'{image_link}\n')
     img_data = requests.get(image_link).content
-    with open(f'{DOWNLOAD_PATH}/{item.post.uri.split("/")[-1]}.png', 'wb') as handler:
+    filename = f'{item.post.uri.split("/")[-1]}.png'
+
+    # If an image of the same filename is in the download path, skip it
+    if filename in listdir(DOWNLOAD_PATH):
+      print(f"Skipping {filename}, already downloaded")
+      duplicates.append(filename)
+      continue
+
+    # Download the image
+    with open(f'{DOWNLOAD_PATH}/{filename}', 'wb') as handler:
       handler.write(img_data)
+
+  print(f"Finished!")
 
 if __name__ == '__main__':
   main()
